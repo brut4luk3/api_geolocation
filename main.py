a3e20b7fa04fc6a26f8b0987f84b52e47c513a25
brut4luk3 <lucasreinert96@gmail.com>
2023-09-20 16:52:45 -0300

    Commit - Mudei pra False

--- main.py
+++ main.py
@@ -66,4 +66,4 @@ def validar_dados(latitude, longitude):
     return True
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=False, port=os.getenv("PORT", default=5000))
